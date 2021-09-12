495553c122c5b8bd7eb4524e473563bec2ff87dc
swynnejr <saucersam@gmail.com>
2021-09-12 00:03:26 -0500

    Register & Logout functionalty

--- src/Header.js
+++ src/Header.js
@@ -7,9 +7,17 @@ import BusinessCenterIcon from '@material-ui/icons/BusinessCenter'
 import ChatIcon from '@material-ui/icons/Chat'
 import NotificationsIcon from '@material-ui/icons/Notifications'
 import HeaderOption from './HeaderOption';
+import { useDispatch }  from "react-redux";
+import { auth } from './firebase';
+import { logout } from './features/userSlice';
 
 
 function Header() {
+    const dispatch = useDispatch();
+    const logoutOfApp = () => {
+        dispatch(logout())
+        auth.signOut();
+    }
     return (
         <div className="header">
             <div className="header__left">
@@ -25,7 +33,11 @@ function Header() {
                 <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
                 <HeaderOption Icon={ChatIcon} title="Messaging"/>
                 <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
-                <HeaderOption avatar="https://assets.dmagstatic.com/wp-content/uploads/2019/04/Wynne-Sam_Braindead-330x220.jpg" title='Me'/>
+                <HeaderOption
+                    avatar="https://assets.dmagstatic.com/wp-content/uploads/2019/04/Wynne-Sam_Braindead-330x220.jpg"
+                    title='Me'
+                    onClick={logoutOfApp}
+                />
             </div>
         </div>
     )
--- src/Login.js
+++ src/Login.js
@@ -1,26 +1,71 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
 import { auth } from './firebase';
+import { login } from './features/userSlice';
 import './Login.css'
 
 function Login() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [profilePic, setProfilePic] = useState("");
+    const dispatch = useDispatch();
 
     const loginToApp = (e) => {
         e.preventDefault();
     };
 
-    const register = () => {};
+    const register = () => {
+        if (!name) {
+            return alert("Please enter a full name!")
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+        .then((userAuth) => {
+            userAuth.user.updateProfile({
+                displayName: name,
+                photoURL: profilePic,
+            })
+            .then(() => {
+                dispatch(
+                    login({
+                        email: userAuth.user.email,
+                        uid: userAuth.user.uid,
+                        displayName: name,
+                        photoURL: profilePic,
+                }))
+            })
+        }).catch((error) => alert(error))
+    };
     return (
         <div className="login">
             <img src="https://logos-download.com/wp-content/uploads/2016/03/LinkedIn_Logo_2011-700x177.png" alt="LinkedIN" />
 
             <form action="">
-                <input placeholder="Full name (Required if registering)" type="text" />
-                <input placeholder="Profile Pic URL (optional)" type="text" />
-                <input placeholder="Email" type="text" />
-                <input placeholder="Password" type="password" />
+                <input
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    placeholder="Full name (Required if registering)"
+                    type="text"
+                />
+                <input
+                    value={profilePic}
+                    onChange={e => setProfilePic(e.target.value)}
+                    placeholder="Profile Pic URL (optional)"
+                    type="text"
+                />
+                <input
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder="Email"
+                    type="text"
+                />
+                <input
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder="Password"
+                    type="password"
+                />
                 <button type="submit" onClick={loginToApp}>Sign In</button>
             </form>
 
