9b0e1dcffa1465b72907ae0a85fc735064626214
swynnejr <saucersam@gmail.com>
2021-09-13 23:34:03 -0500

    Changed News API to media stack

--- src/Widgets.js
+++ src/Widgets.js
@@ -7,10 +7,8 @@ import axios from 'axios';
 function Widgets() {
     const [responseData, setResponseData] = useState([]);
     useEffect(()=>{
-        axios.get('https://newsapi.org/v2/top-headlines?' +
-                  'country=us&' +
-                  'apiKey=351c2c8de7864a768021f65bacc2eea3')
-            .then((response)=>{setResponseData(response.data.articles)})
+        axios.get('http://api.mediastack.com/v1/news?access_key=b1021b516097a964d63afd74bf74a60b&categories=technology&languages=en&sort=published_desc')
+            .then((response)=>{setResponseData(response.data.data)})
     }, []);
     console.log(responseData);
 
@@ -31,8 +29,8 @@ function Widgets() {
                 <h2>LinkedIn News</h2>
                 <InfoIcon />
             </div>
-            {responseData.slice(0,4).map((articles, index)=>{
-                return (<div key={index}><a  href={articles.url}>{newsArticle(articles.title, articles.author)}</a></div>)
+            {responseData.slice(0,5).map((articles, index)=>{
+                return (<div key={index}><a  href={articles.url}>{newsArticle(articles.title, articles.source)}</a></div>)
             })}
         </div>
     )
